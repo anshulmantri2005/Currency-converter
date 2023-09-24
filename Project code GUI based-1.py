340b5e28ee833abe4637128eaf2e9a00d940fd17
Madhav Madan <122387627+MadhavMadan336@users.noreply.github.com>
2023-09-24 19:36:08 +0530

    Update Project code GUI based-1.py

--- Project code GUI based-1.py
+++ Project code GUI based-1.py
@@ -1,3 +1,5 @@
+
+
 # importing everything from tkinter
 from tkinter import *
 
@@ -12,7 +14,8 @@ cor1 = "#333333"     #BLACK
 cor2 = "#E85051"     #RED
 cor3 = "#57FF53"     #GREEN
 cor4 = "#0000ff"     #BLUE
-
+cor5 = "#00F5FF	"    #PURPLE
+cor6 = "#708090	"    #SLATEGRAY
 
 # creating the main window
 window = Tk()
@@ -30,12 +33,56 @@ top.grid(row=0,column=0)
 
 main = Frame(window,width=300,height=260,bg=cor0)
 main.grid(row=1,column=0)
+def convert():
+   #API
+    import requests
 
-#logo of the currency converter
+    url = "https://currency-converter18.p.rapidapi.com/api/v1/convert"
+    currency_1 = combo1.get()
+    currency_2 = combo2.get()
+    amount = value.get()
+
+    querystring = {"from": currency_1, "to": currency_2,"amount": amount}
+    if currency_2 == "INR":
+        symbol = "₹"
+    elif currency_2 == "CNY":
+        symbol = "¥"
+    elif currency_2 == "EUR":
+        symbol = "€"
+    elif currency_2 == "JPY":
+        symbol = "¥"
+    elif currency_2 == "PKR":
+        symbol = "₨"
+    elif currency_2 == "RUB":
+        symbol = "₽"
+    elif currency_2 == "USD":
+        symbol = "$"
+    elif currency_2 == "BDT":
+        symbol = "৳"
+    elif currency_2 == "KZT":
+        symbol = "лв"
+    elif currency_2 == "OMR":
+        symbol = "﷼"
+
+
+    headers = {
+        "X-RapidAPI-Key": "329fbc4bc5msh132f4164bed55fap15edfejsne2dd3fc3aaba",
+        "X-RapidAPI-Host": "currency-converter18.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+    data = json.loads(response.text)
+    converted_amount = data["result"]["convertedAmount"]
+    formatted = symbol + "{:,.2f}".format(converted_amount)
+    result["text"] = formatted
+
+    print(converted_amount, formatted)
+
+#top frame
 icon = Image.open("icon2.jpg")
 icon = icon.resize((40,40))
 icon = ImageTk.PhotoImage(icon)
-#we create name of this gui 
+
 app_name = Label(top,image=icon, compound=LEFT, text="Currency Converter",height=5,padx=9,pady=25,anchor=CENTER,font=("arial 16 bold"),bg=cor3,fg=cor1)
 app_name.place(x=-9,y=0)
 
@@ -43,6 +90,7 @@ app_name.place(x=-9,y=0)
 #main frame
 result = Label(main,text=" ",width=16,height=2,pady=7,relief="solid",anchor=CENTER,font=("Ivy 15 bold"),bg=cor0,fg=cor2)
 result.place(x=50,y=10)
+
 #NAME AND CODE OF DIFFERENT COUNTRY CURRENCY
 currency = ["INR","CNY","EUR","JPY","PKR","RUB","USD","BDT","KZT","OMR"]
 
@@ -62,5 +110,8 @@ combo2.place(x=155,y=115)
 
 value = Entry(main,width=22,justify=CENTER,font=("Ivy 12 bold"),relief=SOLID)
 value.place(x=50,y=155)
+#BUTTON CREATE
+button = Button(main,text="Convert",width=19,padx=5,height=1,bg=cor2,fg=cor0,font=("Ivy 12 bold"),command=convert)
+button.place(x=50,y=210)
 
 window.mainloop()
